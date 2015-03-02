fcb04b43f1d6074bd01c72dad1f7df5a17ca41b0
Aarón Negrín <negrinaaron@gmail.com>
2015-03-02 15:16:36 +0100

    Old assets deletion

--- src/androidtest/assets/basicvs.glsl
+++
@@ -1,9 +0,0 @@
-uniform mat4 mvpMatrix;
-uniform mat4 model;
-
-attribute vec3 position;
-
-void main() {
-	gl_Position = mvpMatrix * model * vec4(position ,1);
-	//gl_Position = vec4(position ,1);
-}
